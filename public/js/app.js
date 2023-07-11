731c8ee1235156b6c1d51bd14b85ccbe81891d92
Gerardzsr22 <gerardsanchezribot@gmail.com>
2023-07-11 12:27:19 +0200

    11-7-23

--- public/js/app.js
+++ public/js/app.js
@@ -4,6 +4,21 @@ let closeButton = document.querySelector(".close-button");
 let modal = document.querySelector(".modal")
 let chekButton = document.querySelector(".checkout")
 
+
+
+let desButton = document.querySelector(".featured-desplegable-visible-button")
+let desplegable = document.querySelector(".featured-desplegable")
+let desplegableNOV = document.querySelector(".featured-desplegable-novisible")
+
+
+desButton.addEventListener("click", () => {
+    desplegable.classList.toggle("active")
+    desplegableNOV.classList.toggle("active")
+    desButton.classList.toggle("active")
+    
+});
+
+
 modalButtons.forEach(modalButton => {
     modalButton.addEventListener("click", () => {
         overlay.classList.add("active"); 
@@ -11,7 +26,7 @@ modalButtons.forEach(modalButton => {
     });
 })
 
-overlay.addEventListener("click",() => {
+overlay.addEventListener("click", () => {
     overlay.classList.remove("active");
     modal.classList.remove("active");
     
